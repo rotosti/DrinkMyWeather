862ef3afd02fc6f3e37510b071459c6a3b11ca7b
Tom Siemion <tsiemion07@gmail.com>
2021-04-13 11:56:44 -0500

    working weather api

---
+++ assets/javascript/script.js
@@ -0,0 +1,57 @@
+var zipCode = '60630';
+var countryCode = 'US'
+
+var apiKey = 'a6bf4e0e3ad10827dd4efb76de3ab5e4';
+var url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},${countryCode}&appid=${apiKey}`;
+
+var searchBtn = $('#searchButton');
+
+var weather = {
+    city: "",
+    img: "",
+    imgDesc: "",
+    temp: "",
+    humidity: "",
+    currentConditions: ""
+}
+
+function getWeatherData() {
+
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+
+            weather.city = data.name;
+            weather.temp = convertKelvin(data.main.temp);
+            weather.humidity = data.main.humidity;
+            weather.img = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            weather.imgDesc = data.weather[0].description;
+            weather.currentConditions = data.weather[0].main;
+
+            console.log(weather);
+            updateCityWeatherCard();
+
+        });
+
+}
+
+searchBtn.on('click', getWeatherData);
+
+function convertKelvin(temp) {
+    return Math.floor((temp - 273.15) * (9/5) + 32);
+}
+
+function updateCityWeatherCard() {
+
+    $('#city-data').empty();
+
+    $('#city-data').append(`<li>${weather.city}</li>` +
+                           `<li><img src="${weather.img}" alt="${weather.imgDesc}"></li>` +
+                           `<li>${weather.currentConditions}</li>` +
+                           `<li>Temp: ${weather.temp} &#730F</li>` +
+                           `<li>Humidity: ${weather.humidity}%</li>`);
+
+}
