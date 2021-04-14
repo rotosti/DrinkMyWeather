2d3227443edbc1ee1d385eb711132a3baa07ab91
jj espinoza <j.j.espinozagomez@gmail.com>
2021-04-14 10:56:30 -0500

    progress

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,8 @@
+// var drinkRecommend = document.getElementById('')
+var neat = ['Siembra Valles 92 proof Blanco Tequila', 'Hakushu 18-Year Whiskey', 'NY Distilling Ragtime Rye', 'Malort', 'Del Maguey Mezcal', 'calvados'];
+var availableBank = [];
+
+
 fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=bourbon')
     .then(function(response) {
         return response.json();
@@ -6,3 +11,10 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=bourbon')
         console.log(data);
     })
 
+    $("p").on("click", function(){
+        alert("The paragraph was clicked.");
+      }); 
+
+    // if(50degrees ==true ){
+    //     availableBank = availableBank.concat(neat)
+    // }
